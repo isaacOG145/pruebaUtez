1bd609430fd063e892c3c35120753a6112623857
isaacOG145 <20233tn145@utez.edu.mx>
2024-06-11 13:56:15 -0600

    pom

---
+++ src/main/java/mx/edu/utez/demogit/utils/DatabaseConecctionManager.java
@@ -0,0 +1,47 @@
+package mx.edu.utez.demogit.utils;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DatabaseConnecctionManager {
+    private static final String JDBC_URL =
+
+            "jdbc:mysql://localhost:3306/pruebaGit";
+
+    private static final String USERNAME =
+
+            "root";
+
+    private static final String PASSWORD =
+
+            "root";
+
+    private static final HikariConfig config = new HikariConfig();
+    private static final HikariDataSource dataSource;
+
+    static {
+        try { Class.forName("com.mysql.cj.jdbc.Driver"/"oracle.jdbc.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Error", e);}
+    }
+        config.setJdbcUrl(JDBC_URL);
+        config.setUsername(USERNAME);
+        config.setPassword(PASSWORD);
+// Ajustes del pool
+        config.setMinimumIdle(5);
+        config.setMaximumPoolSize(10);
+        config.setConnectionTimeout(30000); // 30 seconds
+    dataSource = new HikariDataSource(config);
+}
+
+public static Connection getConnection() throws SQLException {
+    return dataSource.getConnection();
+}
+
+private DatabaseConnecctionManager() {
+// Private constructor to prevent instantiation
+}
+
+}
